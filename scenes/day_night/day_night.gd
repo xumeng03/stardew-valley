11e35ad6c7227135d3d673baa42774a59ac98cd7
xumeng03 <xumeng03@qq.com>
2025-12-21 20:02:58 +0800

    day end

--- scenes/day_night/day_night.gd
+++ scenes/day_night/day_night.gd
@@ -1,38 +1,43 @@
 extends Node2D
 
-@onready var day_duration: float = $Timer.wait_time
-@export var dawn_start: float = 0.25
-@export var dawn_duration: float = 1.0/24.0
-@export var dusk_start: float = 0.75
-@export var dusk_duration: float = 1.0/24.0
+@onready var timer := $Timer
+@onready var day_duration: float = timer.wait_time
+@onready var player = get_tree().get_first_node_in_group("player")
+@export_range(0, 1) var dawn_start: float = 0.25
+@export_range(0, 1) var dawn_duration: float = 1.0 / 24.0
+@export_range(0, 1) var dusk_start: float = 0.75
+@export_range(0, 1) var dusk_duration: float = 1.0 / 24.0
 
-var dawn_color := Color(1.0, 0.8, 0.6, 1.0) # 黎明：温暖的橙色
-var day_color := Color(1.0, 1.0, 1.0, 1.0) # 白天：白色（正常）
-var dusk_color := Color(0.8, 0.6, 0.7, 1.0) # 黄昏：偏红的暗色
-var night_color := Color(0.3, 0.3, 0.5, 1.0) # 夜晚：偏蓝的暗色
-
-var current_time: float = 0.0
+var dawn_color := Color(1.0, 0.8, 0.6, 1.0)
+var day_color := Color(1.0, 1.0, 1.0, 1.0)
+var dusk_color := Color(0.8, 0.6, 0.7, 1.0)
+var night_color := Color(0.3, 0.3, 0.5, 1.0)
 
 func _ready() -> void:
-	current_time = day_duration * dawn_start
+	timer.start(day_duration * (1 - dawn_start))
 
-func _physics_process(delta: float) -> void:
-	current_time += delta
-	if current_time >= day_duration:
-		current_time = 0.0
+func _physics_process(_delta: float) -> void:
+	if timer.time_left < 0:
+		return
+
+	var time_right: float = day_duration - timer.time_left
+
 	var color: Color
-	print(round(current_time))
-	if current_time >= day_duration * dawn_start and current_time < day_duration * dawn_start + day_duration * dawn_duration:
-		# 黎明时间
+	print(round(time_right))
+	if time_right >= day_duration * dawn_start and time_right < day_duration * dawn_start + day_duration * dawn_duration:
 		color = dawn_color
-
-	elif current_time > day_duration * dawn_start + day_duration * dawn_duration and current_time < day_duration * dusk_start:
-		# 白天时间
+	elif time_right >= day_duration * dawn_start + day_duration * dawn_duration and time_right < day_duration * dusk_start:
 		color = day_color
-	elif current_time >= day_duration * dusk_start and current_time < day_duration * dusk_start + day_duration * dusk_duration:
-		# 黄昏时间
+	elif time_right >= day_duration * dusk_start and time_right < day_duration * dusk_start + day_duration * dusk_duration:
 		color = dusk_color
 	else:
-		# 夜晚时间
 		color = night_color
 	$CanvasModulate.color = color
+
+
+func _on_timer_timeout() -> void:
+	var tween := create_tween()
+	tween.tween_property((player.get_node("ColorRect") as ColorRect).material, "shader_parameter/progress", 0.0, 1)
+	tween.tween_interval(15)
+	tween.tween_callback(func(): timer.start(day_duration * (1 - dawn_start)))
+	tween.tween_property((player.get_node("ColorRect") as ColorRect).material, "shader_parameter/progress", 1.0, 1)
